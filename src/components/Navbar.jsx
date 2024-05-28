62ecdba3fb1160d482d327927e68c827e43b4ae4
vasu74 <vasug7409@gmail.com>
2024-05-29 00:57:52 +0530

    rsp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,15 +20,15 @@ const Navbar = () => {
         <SearchInput />
       </div>
       {/* Third Menu */}
-      <div className="flex items-center justify-around gap-6 shadow-sm hover:shadow-md px-6  py-2 rounded-full  ">
+      <div className="flex items-center justify-around gap-8 shadow-sm hover:shadow-md px-6  py-2 rounded-full  ">
         <div className="relative inline-block group">
-          <a href="#" className="px-4 py-2">
+          <a href="#" className="">
             Notification
           </a>
           <span className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out group-hover:scale-x-100" />
         </div>
         <div className="relative inline-block group">
-          <a href="#" className="px-4 py-2">
+          <a href="#" className="">
             Earning
           </a>
           <span className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out group-hover:scale-x-100" />
